ab316f3155b4dc51abac57948a6a556309939fac
Heiko Liebscher <hliebscher@idn.de>
2025-06-09 23:35:12 +0200

    add

---
+++ components/lin_control/__init__.py
@@ -0,0 +1,7 @@
+from typing import Optional
+
+import esphome.codegen as cg
+import esphome.config_validation as cv
+import esphome.final_validate as fv
+from esphome import pins, automation
+from esphome.components import uart, time
